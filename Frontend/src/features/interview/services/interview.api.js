626c876617f72ef000b04955792b5d43630bb64c
Asmit2464-dev <bhattasmit2464@gmail.com>
2026-05-18 17:15:16 +0530

    updated production backend url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL:  "https://careerpilot-ai-zhgz.onrender.com",
     withCredentials: true,
 })
 
