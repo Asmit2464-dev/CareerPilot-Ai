adfdaeefb61b8b4e7e84efa114cfcfed78052bb5
Asmit2464-dev <bhattasmit2464@gmail.com>
2026-05-18 17:30:59 +0530

    fixed render proxy auth cookies

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 const app = require('./src/app');
+app.set("trust proxy", 1) // trust first proxy
 const connectDB = require('./src/config/database');
 
 
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,10 +42,11 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-   res.cookie("token", token, {
+res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "None"
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000
 })
 
 
@@ -95,7 +96,8 @@ async function loginUserController(req, res) {
    res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "None"
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000
 })
     res.status(200).json({
         message: "User loggedIn successfully.",
